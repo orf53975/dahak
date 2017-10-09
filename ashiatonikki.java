6ed4613873057a5ac208d48be0db2e56f19ed0e6
_c0da_ <duvictor514@gmail.com>
2017-10-09 15:04:40 -0700

    Update ashiatonikki.java

--- ashiatonikki.java
+++ ashiatonikki.java
@@ -9,7 +9,7 @@ public class ashiatonikki
   public static void main(String[] args) throws IOException, URISyntaxException
   {
 	  System.out.println("-+-[[KitsuneReceiver Rainier Version (Stable v0.5)]]-+-");
-	  System.out.println("-+-[[Linux Version (for Kali/Debian/Ubuntu)]]-+-");
+	  System.out.println("-+-[[Linux Version (for Kali/Debian/Ubuntu/Fedora)]]-+-");
 	  if (!(System.getProperty("user.name").matches("root"))) {
 		  System.err.println("[!] w(°ｏ°)w You are not the admin. History and Login Data uploader will be unavailable, as they require restricted ports 69 and 420.");
 	  } 
@@ -225,6 +225,26 @@ public class ashiatonikki
 			  }
 		  } else if (ch.matches("ip")) {
 			  System.out.println("IP: " + InetAddress.getLocalHost().getHostAddress());
+		  } else if (ch.matches("version")) {
+			  System.out.println("Update version v0.5");
+		  } else if (ch.matches("license")) {
+			  System.out.println("Permission is hereby granted, free of charge, to any person obtaining a copy\r\n" + 
+			  		"of this software and associated documentation files (the \"Software\"), to deal\r\n" + 
+			  		"in the Software without restriction, including without limitation the rights\r\n" + 
+			  		"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\r\n" + 
+			  		"copies of the Software, and to permit persons to whom the Software is\r\n" + 
+			  		"furnished to do so, subject to the following conditions:\r\n" + 
+			  		"\r\n" + 
+			  		"The above copyright notice and this permission notice shall be included in all\r\n" + 
+			  		"copies or substantial portions of the Software.\r\n" + 
+			  		"\r\n" + 
+			  		"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\r\n" + 
+			  		"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\r\n" + 
+			  		"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\r\n" + 
+			  		"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\r\n" + 
+			  		"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\r\n" + 
+			  		"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\r\n" + 
+			  		"SOFTWARE.");
 		  }
 	  }
   }

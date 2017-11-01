349b568b31faad310eab8f407eb6689642159e18
_c0da_ <duvictor514@gmail.com>
2017-11-01 14:47:24 -0700

    Fixed missing debugger

---
+++ src/main/Chocolat.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Chocolat {
+	static boolean verboseLogging = true;
+	public static void print(String sk) 
+	{
+		if (verboseLogging)
+		{
+			System.out.print(sk);
+		}
+	}
+	public static void println(String sk) 
+	{
+		if (verboseLogging)
+		{
+			System.out.println(sk);
+		}
+	}
+}
